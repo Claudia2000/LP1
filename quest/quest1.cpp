94a2779b182af587af7bc16ee8810cc678d5af5c
Claudia2000 <42683698+Claudia2000@users.noreply.github.com>
2018-10-04 22:16:28 -0500

    Add files via upload

---
+++ quest/quest1.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<algorithm>
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+  string numeros;
+  cout<<"Ingrese dos números enteros"<<endl;
+  while (numeros!="#"){
+    cin>>numeros;
+    cout<<numeros;
+  } 
+
+}
---
+++ quest/quest2.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<algorithm>
+#include <iostream>
+using namespace std;
+
+int main(){
+  int numeros[2]={};
+  cout<<"Ingrese un numero entero"<<endl;
+  cin>>numeros[0];
+  cout<<"Ingrese otro numero entero"<<endl;
+  cin>>numeros[1];
+  
+    if (numeros[0]<numeros[1]){
+      cout<<"El menor valor es "<<numeros[0]<<endl;
+      cout<<"El mayor valor es "<<numeros[1]<<endl;
+    }
+    else {
+      cout<<"El menor valor es "<<numeros[1]<<endl;
+      cout<<"El mayor valor es "<<numeros[0]<<endl;
+    }
+    
+   
+
+}
